93497321c5fae98fcac20d341de6a4e733a34989
EngNelson <bechem200@gmail.com>
2022-10-19 17:38:12 +0100

    about page in progress

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,9 +1,17 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/family-header.png'
 import './about.css'
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <Header title="About Us" image={HeaderImage}>
+    Children Reasurrance Ministry is an NGO, that is set to reachout.
+    To many across the globe with the focus of equiping, empowering,
+    human life. Every Childs destiny counts!!! 
+    </Header>
+    </>
   )
 }
 
