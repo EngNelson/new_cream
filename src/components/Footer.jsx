cbb211d0a2443107fc05988636a4c7ff270f2233
EngNelson <bechem200@gmail.com>
2022-10-19 08:18:47 +0100

    home page complated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom"
 import Logo from "../images/cream.jpg"
 import { FaLinkedin } from "react-icons/fa"
-import { FaFacebook } from "react-icons/fa"
+import { FaFacebookF } from "react-icons/fa"
 import { AiOutlineTwitter } from "react-icons/ai"
 import { AiFillInstagram } from "react-icons/ai"
 
@@ -14,11 +14,12 @@ const Footer = () => {
                     <img src={Logo} alt="Footer" />
                 </Link>
                 <p>
-
+                Reaching out to as many as possible, with care and compassion.
+                As Christ love the world.
                 </p>
                 <div className="footer__socials">
                     <a href="https://linkedin.com/" target="_blank" rel='noreferrer noopener'><FaLinkedin/></a>
-                    <a href="https://facebook.com/" target="_blank" rel='noreferrer noopener'><FaFacebook/></a>
+                    <a href="https://facebook.com/" target="_blank" rel='noreferrer noopener'><FaFacebookF/></a>
                     <a href="https://twitter.com/" target="_blank" rel='noreferrer noopener'><AiOutlineTwitter/></a>
                     <a href="https://instagram.com/" target="_blank" rel='noreferrer noopener'><AiFillInstagram/></a>
                 </div>
@@ -26,11 +27,27 @@ const Footer = () => {
             <article>
                 <h4>Permalinks</h4>
                 <Link to="/about">About</Link>
-                <Link to="/about">About</Link>
-                <Link to="/about">About</Link>
-                <Link to="/about">About</Link>
-                <Link to="/about">About</Link>
+                <Link to="/plans">Plans</Link>
+                <Link to="/trainers">Trainers</Link>
+                <Link to="/gallery">Gallery</Link>
+                <Link to="/contact">Contact</Link>
             </article>
+            <article>
+                <h4>Insights</h4>
+                <Link to="/s">Blog</Link>
+                <Link to="/s">Case Studies</Link>
+                <Link to="/s">Events</Link>
+                <Link to="/s">Communities</Link>
+                <Link to="/s">FAQs</Link>
+            </article>
+            <article>
+                <h4>Get In Touch</h4>
+                <Link to="/contact">Contact Us</Link>
+                <Link to="/s">Support</Link>
+            </article>
+        </div>
+        <div className="footer__copyright">
+            <small>2022 NELSON BECHEM &copy;All Rights Reserved.</small>
         </div>
     </footer>
   )
