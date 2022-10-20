1c84de61c43e226df290bfd4ceaaf8043bcc3646
EngNelson <bechem200@gmail.com>
2022-10-20 11:51:00 +0100

    about gallery finish

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,8 +1,30 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/values.png'
+import {MdEmail} from 'react-icons/md'
+import {BsMessenger} from 'react-icons/bs'
+import {IoLogoWhatsapp} from 'react-icons/io'
+
+
 import './contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <>
+      <Header title="Get In Touch" image={HeaderImage}>
+      Children Reasurrance Ministry is an NGO, that is set to reachout.
+      To many across the globe with the focus of equiping, empowering,
+      human life. Every Childs destiny counts!!! 
+      </Header>
+      <section className="contact">
+        <div className="container contact__container">
+          <div className="contact__wrapper">
+            <a href="mailto:support@bechem200.com" target="_blank" rel="noreferrer noopener"><MdEmail/></a>
+            <a href="http://m.me" target="_blank" rel="noreferrer noopener"><BsMessenger/></a>
+            <a href="https://wa.me/+23767251082" target="_blank" rel="noreferrer noopener"><IoLogoWhatsapp/></a>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/pages/gallery/Gallery.jsx
+++ src/pages/gallery/Gallery.jsx
@@ -1,8 +1,37 @@
+import Header from '../../components/Header'
+import HeaderImage from '../../images/gallery1.jpg'
+
 import './gallery.css'
 
 const Gallery = () => {
+  const galleryLength = 30;
+  const images = []
+
+  for(let i = 1; i <= galleryLength; i++){
+    images.push(require(`../../images/gallery${i}.jpg`))
+  }
+  
+ 
+
   return (
-    <div>Gallery</div>
+    <>
+    <Header  title="Our Gallery" image={HeaderImage}>
+    Children Reasurrance Ministry is an NGO, that is set to reachout.
+    To many across the globe with the focus of equiping, empowering,
+    human life. Every Childs destiny counts!!! 
+    </Header>
+    <section className="gallery">
+      <div className="container gallery__container">
+        {
+          images.map((image, index) => {
+            return <article key={index}>
+              <img src={image} alt={`Gallery Img ${index + 1}`} />
+            </article>
+          })
+        }
+      </div>
+    </section>
+    </>
   )
 }
 
